ed639dc5121c45d8584a33d61cb426e0f767366a
Imperion <ta9494@yandex.ru>
2023-09-27 14:22:36 +0300

    add new file

---
+++ src/pages/HomePage/components/Section/SwimmingClass/data.Class.jsx
@@ -0,0 +1,12 @@
+export const dataСourse = {
+    price:{
+        children: "от 1250₽",
+        adult: "от 1250₽",
+        personality: "от 1900₽"
+    },
+    name:{
+        children: "Детские групповые",
+        adult: "Взрослые групповые",
+        personality: "Персональные" 
+    }
+}
