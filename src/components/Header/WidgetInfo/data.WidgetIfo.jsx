d74b942201acaf4ca6b0271f719e3da4b1cf0286
Imperion <ta9494@yandex.ru>
2023-11-29 14:49:42 +0300

    add  picture and logo new

---
+++ src/components/Header/WidgetInfo/data.WidgetIfo.jsx
@@ -0,0 +1,8 @@
+
+export const dataWidgetIfo = {
+    description: "",
+    adress: "г. Москва, напишите вашу локацию, и мы подберем для вас бассейн",
+    mail:"Hellostrela@gmail.com",
+    phone:"8-906-055-40-13",
+    time:"Ежедневно с 07:00 – 23:00",
+}
